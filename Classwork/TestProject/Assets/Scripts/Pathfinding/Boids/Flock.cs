25d298b61eca8298d3f0fe00a559d3fcafe3af2f
Dylan <dylanwebb5@hotmail.com>
2020-02-22 00:57:03 +1100

    Boids Flock Setup

---
+++ Classwork/TestProject/Assets/Scripts/Pathfinding/Boids/Flock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Pathfinding.Boids
+{
+    public class Flock : MonoBehaviour
+    {
+        [Header("Flock Setup")]
+        public FlockAgent agentPrefab;
+        List<FlockAgent> agents = new List<FlockAgent>();
+        public FlockBehaviour behaviour;
+
+        [Range(10, 500)] public int startingCount = 100;
+        private const float AgentDensity = 0.8f;
+
+        [Header("Agent Stats")] 
+        [Range(1f, 100f)] 
+        public float driveFactor = 10f;
+        [Range(1f, 100f)] 
+        public float maxSpeed = 5f;
+        [Range(1f, 10f)] 
+        public float neightbourRadius;
+        [Range(0f, 1f)] 
+        public float avoidanceRadiusMultiplier = 0.5f;
+
+        private float squareMaxSpeed;
+        private float squareNeighbourRadius;
+        private float squareAvoidanceRadius;
+
+        public float SquareAvoidanceRadius
+        {
+            get { return squareAvoidanceRadius; }
+        }
+
+        private void Start()
+        {
+            squareMaxSpeed = maxSpeed * maxSpeed;
+            squareNeighbourRadius = neightbourRadius * neightbourRadius;
+            squareAvoidanceRadius = squareNeighbourRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+            for (int i = 0; i < startingCount; i++)
+            {
+                Vector3 newPos = Random.insideUnitSphere * startingCount * AgentDensity;
+                FlockAgent newAgent = Instantiate(agentPrefab, new Vector3(newPos.x,transform.position.y + 1,newPos.z),
+                    Quaternion.Euler(Vector3.up * Random.Range(0f, 360)), transform);
+                newAgent.name = "Agent " + i;
+                agents.Add(newAgent);
+            }
+        }
+    }
+}
---
+++ Classwork/TestProject/Assets/Scripts/Pathfinding/Boids/FlockAgent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pathfinding.Boids
+{
+    [RequireComponent(typeof(Collider))]
+    public class FlockAgent : MonoBehaviour
+    {
+        private Collider agentCollider;
+
+        public Collider AgentCollider
+        {
+            get
+            {
+                return agentCollider;
+            }
+        }
+
+        private void Start()
+        {
+            agentCollider = GetComponent<Collider>();
+        }
+
+        public void Move(Vector3 velocity)
+        {
+            transform.forward = velocity;
+            transform.position += (Vector3)velocity * Time.deltaTime;
+        }
+    }
+}
