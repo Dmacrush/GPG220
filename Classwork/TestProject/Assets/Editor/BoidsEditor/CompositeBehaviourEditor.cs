47ae3c20d99695bb0443935ba8774a9994de5915
Dylan <dylanwebb5@hotmail.com>
2020-02-24 21:32:31 +1100

    Finished Editor Script
    Buttons now appear on the behaviour scriptable objects

--- Classwork/TestProject/Assets/Editor/BoidsEditor/CompositeBehaviourEditor.cs
+++ Classwork/TestProject/Assets/Editor/BoidsEditor/CompositeBehaviourEditor.cs
@@ -1,4 +1,5 @@
-﻿using Pathfinding.Boids.Behaviour_Scripts;
+﻿using Pathfinding.Boids;
+using Pathfinding.Boids.Behaviour_Scripts;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,33 +8,116 @@ namespace BoidsEditor
     [CustomEditor(typeof(CompositeBehaviour))]
     public class CompositeBehaviourEditor : Editor
     {
+        /// <summary>
+        /// Editor stuff for each array field and allows for array changes 
+        /// </summary>
         public override void OnInspectorGUI()
         {
             CompositeBehaviour cb = (CompositeBehaviour) target;
-            
-            Rect r = EditorGUILayout.BeginHorizontal();
-            r.height = EditorGUIUtility.singleLineHeight;
-            
-            //check for behaviour
+
             if (cb.behaviours == null || cb.behaviours.Length == 0)
             {
-                EditorGUILayout.HelpBox("No Behaviour in array." , MessageType.Warning);
-                EditorGUILayout.EndHorizontal();
-                
-                r = EditorGUILayout.BeginHorizontal();
-                r.height = EditorGUIUtility.singleLineHeight;
+                if (GUILayout.Button("Add Behaviour"))
+                {
+                    //Add behaviour
+                    AddBehaviour(cb);
+                    //ensures changes are saved
+                    EditorUtility.SetDirty(cb);
+                }
             }
             else
             {
-                r.x = 30f;
-                r.width = EditorGUIUtility.currentViewWidth - 95f;
-                EditorGUI.LabelField(r,"Behaviours");
-                r.x = EditorGUIUtility.currentViewWidth - 65f;
-                r.width = 60f;
-                EditorGUI.LabelField(r, "Weights");
-                r.y += EditorGUIUtility.singleLineHeight * 1.2f;
-                
+                //sets up the fields for each behaviour
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Number", GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                EditorGUILayout.LabelField("Behaviors", GUILayout.MinWidth(60f));
+                EditorGUILayout.LabelField("Weights", GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                EditorGUILayout.EndHorizontal();
+
+                //check for changes
+                EditorGUI.BeginChangeCheck();
+
+                //sets up the visuals for each array 
+                for (int i = 0; i < cb.behaviours.Length; i++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField(i.ToString(), GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                    cb.behaviours[i] = (FlockBehaviour) EditorGUILayout.ObjectField(cb.behaviours[i],
+                        typeof(FlockBehaviour), false, GUILayout.MinWidth(60f));
+                    cb.weights[i] =
+                        EditorGUILayout.FloatField(cb.weights[i], GUILayout.MinWidth(60f), GUILayout.MaxWidth(60f));
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    EditorUtility.SetDirty(cb);
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+
+                //adding move behaviours
+                EditorGUILayout.BeginHorizontal();
+                if (GUILayout.Button("Add Behaviour"))
+                {
+                    //Add behaviour
+                    AddBehaviour(cb);
+                    //ensures changes are saved
+                    EditorUtility.SetDirty(cb);
+                }
+
+
+                //removes behaviour
+                if (cb.behaviours != null || cb.behaviours.Length > 0)
+                {
+                    if (GUILayout.Button("Remove Behaviour"))
+                    {
+                        //Remove Behaviour
+                        RemoveBehaviour(cb);
+                        //ensures changes are saved
+                        EditorUtility.SetDirty(cb);
+                    }
+                }
             }
+        }
+
+        void AddBehaviour(CompositeBehaviour cb)
+        {
+            int oldCount = (cb.behaviours != null) ? cb.behaviours.Length : 0;
+            FlockBehaviour[] newBehaviours = new FlockBehaviour[oldCount + 1];
+            float[] newWeights = new float[oldCount + 1];
+            for (int i = 0; i < oldCount; i++)
+            {
+                newBehaviours[i] = cb.behaviours[i];
+                newWeights[i] = cb.weights[i];
+            }
+
+            newWeights[oldCount] = 1f;
+            cb.behaviours = newBehaviours;
+            cb.weights = newWeights;
+        }
+
+        void RemoveBehaviour(CompositeBehaviour cb)
+        {
+            int oldCount = cb.behaviours.Length;
+            if (oldCount == 1)
+            {
+                cb.behaviours = null;
+                cb.weights = null;
+                return;
+            }
+
+            FlockBehaviour[] newBehaviours = new FlockBehaviour[oldCount - 1];
+            float[] newWeights = new float[oldCount - 1];
+            for (int i = 0; i < oldCount - 1; i++)
+            {
+                newBehaviours[i] = cb.behaviours[i];
+                newWeights[i] = cb.weights[i];
+            }
+
+            cb.behaviours = newBehaviours;
+            cb.weights = newWeights;
         }
     }
 }
